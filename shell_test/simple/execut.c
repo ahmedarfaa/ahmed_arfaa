f234089b26a7f9ea7565832cf4ffd4fde9f93872
ahmedarfaa <ahmedarfaa594@gmail.com>
2023-05-17 17:56:28 +0300

    last

--- shell_test/simple/execut.c
+++ shell_test/simple/execut.c
@@ -66,6 +66,12 @@ void execute_echo(char **args)
                 printf("%d\n", value);
             }
         }
+        if (_strcmp(args[i], "$?") == 0)
+            {
+                char exit_status[10];
+                sprintf(exit_status, "%d", WEXITSTATUS(status));
+                write(STDOUT_FILENO, exit_status, strlen(exit_status));
+            }
         else
         {
             write(STDOUT_FILENO, args[i], _strlen(args[i]));
