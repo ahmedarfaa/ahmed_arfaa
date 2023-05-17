844083f05eed131e3cdf6fe6acc5810b936c2aa5
ahmedarfaa <ahmedarfaa594@gmail.com>
2023-05-17 23:55:58 +0300

    final

--- shell_test/simple/main.h
+++ shell_test/simple/main.h
@@ -41,5 +41,6 @@ void execute_command_with_redirection(char **args, char *full_path);
 void execute_clear(void);
 char *_strchr(char *s, char c);
 int _sprintf(char *buf, const char *format, ...);
+int execute_command(char **args, char **env, int *status);
 
 #endif
