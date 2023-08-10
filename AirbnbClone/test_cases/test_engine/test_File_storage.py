edd25f7cf3395e54a5e10ab10a86a1f68e610fd5
ahmedarfaa <ahmedarfaa594@gmail.com>
2023-08-10 20:33:55 +0300

    some test edits

--- AirbnbClone/test_cases/test_engine/test_File_storage.py
+++ AirbnbClone/test_cases/test_engine/test_File_storage.py
@@ -88,34 +88,6 @@ class TestFileStorage(unittest.TestCase):
         key = user.__class__.__name__ + "." + str(user.id)
         self.assertIsNotNone(obj[key])
 
-    def test_reload_filestorage(self):
-        """
-        tests reload
-        """
-        self.storage.save()
-        Root = os.path.dirname(os.path.abspath("console.py"))
-        path = os.path.join(Root, "objects.json")
-        with open(path, 'r') as f:
-            lines = f.readlines()
-        try:
-            os.remove(path)
-        except Exception:
-            pass
-        self.storage.save()
-        with open(path, 'r') as f:
-            lines2 = f.readlines()
-        self.assertEqual(lines, lines2)
-        try:
-            os.remove(path)
-        except Exception:
-            pass
-        with open(path, "w") as f:
-            f.write("{}")
-        with open(path, "r") as r:
-            for line in r:
-                self.assertEqual(line, "{}")
-        self.assertIs(self.storage.reload(), None)
-
     def test_pep8_conformance_file_storage(self):
         """Test that we conform to PEP8."""
         pep8style = pep8.StyleGuide(quiet=True)
